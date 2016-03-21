2da2a20d6bbd8e9fc821aee292b92cf2428640a6
ArifCengic <arif.cengic@gmail.com>
2016-03-21 17:24:19 +0100

    Initial commit

--- app/src/main/java/com/example/android/androidcourse2/MainActivity.java
+++ app/src/main/java/com/example/android/androidcourse2/MainActivity.java
@@ -1,31 +1,78 @@
 package com.example.android.androidcourse2;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
 	//main activity
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ACTION_EVENTS", "onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ACTION_EVENTS", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ACTION_EVENTS", "onPause");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ACTION_EVENTS", "onDestroy");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ACTION_EVENTS", "onStop");
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.i("ACTION_EVENTS", "onConfigurationChanged");
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("ACTION_EVENTS", "onCreate");
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      //  setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), PersonalInfoActivity.class));
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
